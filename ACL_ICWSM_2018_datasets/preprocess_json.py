49bc2a9b9efa25c6e5c00a0f769aa91fa15604fa
Luke Snyder <snyderluke8@gmail.com>
2019-11-11 22:23:02 -0500

    Update Nepal preprocessing.

--- ACL_ICWSM_2018_datasets/preprocess_json.py
+++ ACL_ICWSM_2018_datasets/preprocess_json.py
@@ -4,12 +4,13 @@ import csv
 import json
 
 dic = {}
-with open('./data/ids.json.data.json') as fr:
+
+with open('./downloaded_tweets/ids.json.data.json') as fr:
 	for l in fr:
 		jobj = json.loads(l.strip())
 		dic[jobj['id_str']] = {'created_at': jobj['created_at']}
 
-with open('./data/ids.json.label.json') as fr:
+with open('./downloaded_tweets/ids.json.label.json') as fr:
 	for l in fr:
 		jobj = json.loads(l.strip())
 		if dic.get(jobj['tweet_id']) is not None:
@@ -32,27 +33,7 @@ for key, value in dic.items():
 
 print(relevant_dates_dic)
 print(irrelevant_dates_dic)
-# dates = ['2012-10-22', '2012-10-23', '2012-10-24', '2012-10-25', '2012-10-26', '2012-10-27',
-#          '2012-10-28', '2012-10-29', '2012-10-30', '2012-10-31', '2012-11-01', '2012-11-02']
-# dates_dfs = [pd.DataFrame(columns=[column for column in df.columns]) for date in dates]
 
-# for i in range(0, len(dates_dfs)):
-#     dates_dfs[i] = df[df['date'] == dates[i]]
-#     dates_dfs[i] = dates_dfs[i][:40000]
-#     # print(dates_dfs[i][dates_dfs[i]['sandy_keyword'] == True].shape[0])
-#     # print(dates_dfs[i][dates_dfs[i]['sandy_keyword'] == False].shape[0])
-#     # print(' ')
-#     # num_true = dates_dfs[i][dates_dfs[i]['sandy_keyword'] == True]
-#     # sandy = num_true.sample(min(2000, num_true.shape[0]), random_state=47)
-#     # non_sandy = dates_dfs[i][dates_dfs[i]['sandy_keyword'] == False].sample(2000 - sandy.shape[0], random_state=47)
+df = pd.DataFrame.from_dict(dic, orient='index')
+print(df.head())
 
-#     # dates_dfs[i] = pd.concat([sandy, non_sandy])
-#     # print(dates_dfs[i].shape[0])
-#     # print(dates_dfs[i][dates_dfs[i]['sandy_keyword'] == True].shape[0])
-#     # print(dates_dfs[i][dates_dfs[i]['sandy_keyword'] == False].shape[0])
-#     dates_dfs[i] = dates_dfs[i].sample(5000, random_state=47)
-#     # print(dates_dfs[i].shape[0])
-#     # print(dates_dfs[i][dates_dfs[i]['sandy_keyword'] == True].shape[0])
-#     # print(dates_dfs[i][dates_dfs[i]['sandy_keyword'] == False].shape[0])
-#     # print(' ')
-#     # dates_dfs[i] = dates_dfs[i].sample(2000, random_state=47)
