5040cb92db0e34f63c46499b2525bcbe111739e4
Luke S <snyderluke8@gmail.com>
2019-11-13 19:32:13 -0500

    Fix word2vec embedding bug.

--- ACL_ICWSM_2018_datasets/src/models/lr.py
+++ ACL_ICWSM_2018_datasets/src/models/lr.py
@@ -3,14 +3,14 @@ import numpy as np
 import sys
 from word2vec import *
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 
 if __name__=='__main__':	
 	# Read input data.
 	inputfile = sys.argv[1]
-	df = pd.read_csv(inputfile)
+	word2vec_path = sys.argv[2]
 
-	w2v = word2vec()
+	df = pd.read_csv(inputfile)
+	w2v = word2vec(word2vec_path)
 
 	df['tokens'] = w2v.pad(df['text'].tolist())
 	w2v.create_embeddings(df['tokens'])
@@ -19,7 +19,7 @@ if __name__=='__main__':
 	train_data = df.drop(test_data.index)
 	class_column = 'relevance_label'
 
-	lr_model = SVC().fit(
+	lr_model = LogisticRegression(penalty='l2', max_iter=500).fit(
 		w2v.compute_average_weights(train_data['tokens']), train_data[class_column]
 	)
 
--- ACL_ICWSM_2018_datasets/src/models/word2vec.py
+++ ACL_ICWSM_2018_datasets/src/models/word2vec.py
@@ -9,11 +9,10 @@ import gensim
 import tensorflow as tf
 
 class word2vec:
-	def __init__(self):
+	def __init__(self, word2vec_path):
 		self.EMBEDDING_DIM = 300
-		self.word2vec_path = 'C:/Users/lukesnyder/Documents/SMART/DjangoServer/DEV/SMART/static/SMART/data/GoogleNews-vectors-negative300.bin.gz'
-		# self.word2vec = gensim.models.KeyedVectors.load_word2vec_format(self.word2vec_path, binary=True)
-		self.word2vec = []
+		self.word2vec = gensim.models.KeyedVectors.load_word2vec_format(word2vec_path, binary=True)
+		# self.word2vec = []
 
 	# Pad data.
 	def pad(self, data):
@@ -34,20 +33,28 @@ class word2vec:
 
 		for word, index in word_index.items():
 			self.embedding_weights[index, :] = self.word2vec[word] if word in self.word2vec else np.random.rand(self.EMBEDDING_DIM)
-			
+		
 		self.embedding_layer = Embedding(self.embedding_weights.shape[0], self.embedding_weights.shape[1], 
 			input_length=self.MAX_SEQUENCE_LENGTH, weights=[self.embedding_weights], trainable=False)
 
 	# Compute average embedding weights for each data point.
 	def compute_average_weights(self, data):
-		tensor = tf.convert_to_tensor(np.asarray(list(data), np.float32), dtype=tf.float32)
-		embedded = self.embedding_layer(tensor)
-	
-		init_g = tf.global_variables_initializer()
-		init_l = tf.local_variables_initializer()
+		# init_g = tf.compat.v1.global_variables_initializer()
+		# init_l = tf.compat.v1.local_variables_initializer()
 
-		with tf.Session() as sess:
-			sess.run(init_g)
-			sess.run(init_l)
+		# with tf.Session() as sess:
+		# 	sess.run(init_g)
+		# 	sess.run(init_l)
+
+		# 	tensor = tf.convert_to_tensor(np.asarray(list(data), np.float32), dtype=tf.float32)
+		# 	embedded = self.embedding_layer(tensor)
+		# 	print(embedded.eval())
+		# 	print(' ')
 			
-			return np.average(embedded.eval(), axis=1)
+		# 	return np.average(embedded.eval(), axis=1)
+		averages = [
+			np.average([self.embedding_weights[token] for token in tokens], axis=0)
+			for tokens in data
+		]
+
+		return averages
