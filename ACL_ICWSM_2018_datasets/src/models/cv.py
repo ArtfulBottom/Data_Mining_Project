3b6a7f13d2c97f273e80d1a8a3eea40bce354764
Luke S <snyderluke8@gmail.com>
2019-11-24 00:46:27 -0500

    LSTM cv.

--- ACL_ICWSM_2018_datasets/src/models/cv.py
+++ ACL_ICWSM_2018_datasets/src/models/cv.py
@@ -6,9 +6,62 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import normalize
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import Adam
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras import backend as K
+
+def create_w2v_bilstm(learn_rate=0.001, batch_size=64, epochs=4, dropout=0.4):
+	global w2v
+
+	bi_lstm = Sequential()
+	bi_lstm.add(Dense(batch_size, input_shape=(w2v.MAX_SEQUENCE_LENGTH, w2v.EMBEDDING_DIM)))
+	bi_lstm.add(Bidirectional(LSTM(w2v.EMBEDDING_DIM, dropout=dropout, activation='relu')))
+	bi_lstm.add(Dense(1, activation='sigmoid'))
+
+	optimizer = Adam(lr=learn_rate)
+	bi_lstm.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+	return bi_lstm
+
+def create_w2v_time(learn_rate=0.001, dropout=0.4):
+	global w2v
+
+	nn = Sequential()
+	nn.add(Dense(batch_size, input_dim=1, activation='relu'))
+	nn.add(Dropout(dropout))
+	nn.add(Dense(1, activation='sigmoid'))
+
+	optimizer = Adam(lr=learn_rate)
+	nn.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+	return nn
+
+def create_w2v_bilstm_time(learn_rate=0.001, dropout=0.4):
+	global w2v
+
+	bi_lstm = Sequential()
+	bi_lstm.add(Dense(batch_size, input_shape=(w2v.MAX_SEQUENCE_LENGTH, w2v.EMBEDDING_DIM)))
+	bi_lstm.add(Bidirectional(LSTM(w2v.EMBEDDING_DIM, dropout=dropout, activation='relu')))
+	bi_lstm.add(Dense(32, activation='relu'))
+
+	nn = Sequential()
+	nn.add(Dense(batch_size, input_dim=1, activation='relu'))
+	nn.add(Dropout(0.5))
+
+	merged_out = Add()([bi_lstm.output, nn.output])
+	merged_out = Dense(1, activation='sigmoid')(merged_out)
+
+	model = Model([bi_lstm.input, nn.input], merged_out)
+	optimizer = Adam(lr=learn_rate)
+	model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+	return model
 
 def kfold_cv(data, word2vec_path, model_choice):
 	# Setup data: w2v, time, and w2v + time.
+	global w2v 
 	w2v = word2vec(word2vec_path)
 	data['tokens'] = w2v.pad(data['text'].tolist())
 	w2v.create_embeddings(data['tokens'])
@@ -61,6 +114,35 @@ def kfold_cv(data, word2vec_path, model_choice):
 		# Find best parameters for w2v + time.
 		gs.fit(average_weights_time, data[class_label])
 		print('Best parameters for SVM word2vec + time: %r' % gs.best_params_)
+		print('Corresponding accuracy: %.4f' % (gs.best_score_ * 100))
+
+	elif model_choice == 'LSTM':
+		batch_size = [64, 128]
+		epochs = [4, 10]
+		learn_rate = [0.001, 0.01]
+		dropout = [0.4, 0.8]
+
+		param_grid = dict(learn_rate=learn_rate, batch_size=batch_size, epochs=epochs, dropout=dropout)
+
+		weights = w2v.compute_all_weights(data['tokens'])
+
+		w2v_bilstm_model = KerasClassifier(build_fn=create_w2v_bilstm, verbose=0)
+		w2v_time_model = KerasClassifier(build_fn=create_w2v_time, verbose=0)
+		w2v_bilstm_time_model = KerasClassifier(build_fn=create_w2v_bilstm_time, verbose=0)
+
+		gs = GridSearchCV(estimator=w2v_bilstm_model, param_grid=param_grid, cv=2)
+		gs.fit(weights, data[class_label])
+		print('Best parameters for BiLSTM word2vec: %r' % gs.best_params_)
+		print('Corresponding accuracy: %.4f' % (gs.best_score_ * 100))
+
+		gs = GridSearchCV(estimator=w2v_time_model, param_grid=param_grid, cv=2)
+		gs.fit(np.asarray(data['day_label']), data[class_label])
+		print('Best parameters for BiLSTM time: %r' % gs.best_params_)
+		print('Corresponding accuracy: %.4f' % (gs.best_score_ * 100))
+
+		gs = GridSearchCV(estimator=w2v_bilstm_time_model, param_grid=param_grid, cv=2)
+		gs.fit([weights, np.asarray(data['day_label'])], data[class_label])
+		print('Best parameters for BiLSTM word2vec + time: %r' % gs.best_params_)
 		print('Corresponding accuracy: %.4f' % (gs.best_score_ * 100))
 
 if __name__=='__main__':
